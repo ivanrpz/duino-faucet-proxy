1da681e13a3bd46578ebffecd2c7f94b61ce7775
ivanrpz <78346517+ivanrpz@users.noreply.github.com>
2022-04-15 17:45:33 +0200

    send full response

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const { exec } = require("child_process");
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function(req, res) {
     exec(`curl -x ${process.env.PROXY_ADDRESS} --location --request GET 'https://server.duinocoin.com/transaction/?username=ivanrpz&password=${process.env.DUINO_PASSWORD}&recipient=${req.query.user}&amount=${req.query.amount}&memo=Try%20duino-faucet.com!'`, (error, stdout, stderr) => {
-        res.send(`stdout: ${stdout}`);
+        res.send(stdout);
     });
 });
 
